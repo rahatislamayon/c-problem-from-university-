f24c62cfb765218e1d2968cb205013b73bba71d2
rahat islam ayon <rahatislamayon@gmail.com>
2024-10-26 16:02:08 +0600

    Add files via upload

---
+++ spl mid code practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int a=3, b=4, c=-5, result;
+int mod;
+result = a * b % c + b;
+printf("result = %d\n",result);
+if (result >= 0 && result < 10)
+{
+    printf("a =  %d\n", a);
+}
+else if (result >= 5)
+{
+    printf("b =  %d\n", b);
+}
+else {printf("a = %d\n");}
+}
